c100c7b8314f3ac37aef7ae411c808abcd507394
elenaOrtega <elenabaura56@gmail.com>
2020-02-05 16:40:12 +0000

    someDBModifications

--- db/config.js
+++ db/config.js
@@ -2,8 +2,8 @@ const mysql = require('mysql');
 const keys = require('../keys');
 
 const connection = mysql.createConnection({
-    host:  'localhost', // address of the server
-    user: keys.MYSQL_USER,
+    host: MYSQL_HOST, // address of the server
+    user: keys.MYSQL_USERNAME,
     password: keys.MYSQL_PASSWORD,
     database: keys.MYSQL_DATABASE,
 });
